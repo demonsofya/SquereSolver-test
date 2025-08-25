3be7fca3c7379cf429d2916aa2d5543480786381
demonsofya <148910070+demonsofya@users.noreply.github.com>
2025-08-25 16:37:55 +0300

    Перевод комментариев

--- mathematics.cpp
+++ mathematics.cpp
@@ -4,7 +4,7 @@
 #include "mathematics.h"
 #include "solving.h"
 
-bool CompareDoubles(double a, double b) {   /*сравнение даблов */
+bool CompareDoubles(double a, double b) {   /*Comparing double nums */
 
     return (fabs(a - b) < EPS);
 }
